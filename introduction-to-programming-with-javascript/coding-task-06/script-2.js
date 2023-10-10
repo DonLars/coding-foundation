df12e72b54ac196a325caba5e34b9938eae571b7
Lars Wagner <lars@tubies.de>
2023-10-10 23:01:46 +0200

    fix: add split method.

--- introduction-to-programming-with-javascript/coding-task-06/script-2.js
+++ introduction-to-programming-with-javascript/coding-task-06/script-2.js
@@ -1,22 +1,25 @@
-/*02) charCount function
+/*
+02) charCount function
 Implement a charCount function.
 
 ✔ The function has a first parameter for a word which is a string.
 ✔ The function has a second parameter which contains the character to search for which is a string.
 ✔ Check if the second parameter consists of only one character, if not, quit the function.
- The function returns the number of times the character from the second parameter is present in the word.
- The function should be case insensitive.
+✔ The function returns the number of times the character from the second parameter is present in the word.
+✔ The function should be case insensitive.
  */
 
 // TODO: Implement the charCount function
 
 function charCount(string, seekChar) {
-  let times = 0;
+  // check if variable max. 1 character
   if (seekChar.length === 1) {
+    // initalize the variable
+    let times = 0;
+    // iterate each string character
     for (let i = 0; i <= string.length; i++) {
-      if (string.charAt(i) === seekChar) {
-        times + 1;
-      }
+      // use the method split, seek for the length of "seekChar" and subtract 1, because there is an additional Array element
+      times = string.split(seekChar).length - 1;
       return times;
     }
   } else {
